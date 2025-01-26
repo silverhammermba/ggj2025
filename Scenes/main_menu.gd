96524864fb9db17ada9500a3b4afabe0ea9ac992
JayIKarus <61361783+JayIKarus@users.noreply.github.com>
2025-01-26 10:00:43 -0500

    Credits

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 
-
+@onready var creditsBox = %CreditsBox 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MarginContainer/HBoxContainer/VBoxContainer2/StartButton.grab_focus() # Replace with function body.
@@ -18,7 +18,11 @@ func _on_options_button_pressed():
 
 
 func _on_credits_button_pressed():
-	pass # Replace with function body.
+	if creditsBox.is_visible_in_tree() == true:
+		creditsBox.visible = false
+	else:
+		creditsBox.visible = true# Replace with function body.
+	
 
 
 func _on_start_button_pressed():
